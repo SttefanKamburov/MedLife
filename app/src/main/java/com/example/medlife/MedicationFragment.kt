0b8bb523a376ed5601d66c37e3720113e97ae877
Kiril.Hristov <kiril.hristov@mypos.com>
2022-11-16 18:55:44 +0200

    fixes

--- app/src/main/java/com/example/medlife/MedicationFragment.kt
+++ app/src/main/java/com/example/medlife/MedicationFragment.kt
@@ -11,37 +11,24 @@ import com.example.medlife.models.MedicationDto
 import com.example.medlife.repository.MedicationRepository
 import com.example.medlife.ui.adapters.MedicationDtoRecyclerAdapter
 
-
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [medication.newInstance] factory method to
- * create an instance of this fragment.
- */
 class MedicationFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private val medicationsList : ArrayList<MedicationDto> = arrayListOf()
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        getMedications()
-    }
+    private val medicationsList : ArrayList<MedicationDto> = arrayListOf()
+    private lateinit var medicationsAdapter: MedicationDtoRecyclerAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val view: View =  inflater.inflate(com.example.medlife.R.layout.fragment_medication, container, false)
+        val view: View =  inflater.inflate(R.layout.fragment_medication, container, false)
+
+        medicationsAdapter = MedicationDtoRecyclerAdapter(view.context, medicationsList)
+
+        val recyclerView: RecyclerView  = view.findViewById(R.id.medication_fragment_recycler_view)
+        recyclerView.layoutManager      = LinearLayoutManager(activity)
+        recyclerView.adapter            = medicationsAdapter
 
         getMedications()
-
-        val recyclerView: RecyclerView  = view.findViewById(com.example.medlife.R.id.medication_fragment_recycler_view)
-        recyclerView.layoutManager      = LinearLayoutManager(view.context)
-        recyclerView.adapter            = MedicationDtoRecyclerAdapter(view.context, medicationsList)
 
         return view
     }
@@ -49,5 +36,6 @@ class MedicationFragment : Fragment() {
     private fun getMedications(){
         medicationsList.clear()
         medicationsList.addAll(MedicationRepository.getMedicationArray())
+        medicationsAdapter.notifyDataSetChanged()
     }
 }
